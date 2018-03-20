1518a2fe0ce657d2262d711d5a3b3657593cc7ff
Vivek Vishwakarma <viveksallu156@gmail.com>
2018-03-20 15:15:41 +0530

    Create eclat.R

---
+++ ML with R/eclat.R
@@ -0,0 +1,13 @@
+#step1: load the library arules
+
+library(arules)
+
+#read the data
+sm = read.transactions("Market_Basket_Optimisation.csv")
+summary(sm)
+
+itemFrequencyPlot(sm)
+
+rules = eclat(data = sm, parameter = list(support = 0.03, minlen = 3))
+
+inspect(rules)
