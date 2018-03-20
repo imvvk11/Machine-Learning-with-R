b6b4dd4ba2774735e569c26e246851aac25c7ed8
Vivek Vishwakarma <viveksallu156@gmail.com>
2018-03-20 15:05:14 +0530

    Create apriori.R

---
+++ ML with R/apriori.R
@@ -0,0 +1,13 @@
+# step:
+
+#import library arules
+library(arules)
+sm = read.transactions("Market_Basket_Optimisation.csv", sep = ',', rm.duplicates = TRUE)
+summary(sm)
+
+itemFrequencyPlot(sm)
+
+# install.packages('arules')
+rules = apriori(data = sm, parameter = list(support = 0.003, confidence = 0.2))
+sorted_rules = sort(rules, by = 'confidence')
+inspect(sorted_rules[1:10])
